969b398abfedf412ab87b6ccc6ad8276d9329d02
Warren <hello@warren.codes>
2022-06-06 10:42:14 -0600

    Add to routes and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :characters
-  resources :locations
+  resources :characters, only: [:index] # except: []
+  resources :locations, only: [:index, :show]
 end
