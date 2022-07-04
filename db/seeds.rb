1e2ef5d1cc077cd5341540bb2f0f1b3a2fb6bbeb
Warren <hello@warren.codes>
2022-07-04 10:04:11 -0600

    Add database seeds for locations and characters.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating Locations..."
+
+20.times do
+    Location.create(name: Faker::TvShows::RickAndMorty.location)
+end
+
+puts "Creating Chracters..."
+
+locations = Location.all # Assign all locations to variable.
+
+50.times do
+    Character.create(
+        name: Faker::TvShows::RickAndMorty.character,
+        quote: Faker::TvShows::RickAndMorty.quote,
+        location: locations.sample # Get a random location.
+    )
+end
+
+puts "Seeding complete."
