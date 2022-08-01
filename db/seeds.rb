0062b2a0b83b77005c6366dfa40a47054bff6682
Warren <hello@warren.codes>
2022-08-01 10:08:30 -0600

    Add seeds for Location and Character.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Beginning seeds..."
+
+puts "Seeding locations..."
+
+10.times do
+    Location.create(name: Faker::Fantasy::Tolkien.location)
+end
+
+locations = Location.all
+
+puts "Seeding characters.."
+
+30.times do
+    Character.create(
+        name: Faker::Fantasy::Tolkien.character,
+        race: Faker::Fantasy::Tolkien.race,
+        poem: Faker::Fantasy::Tolkien.poem,
+        location: locations.sample
+    )
+end
+
+puts "Seeding completed!"
