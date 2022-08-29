59d88674949c962df5c6f8dacf6845881460460d
Warren <hello@warren.codes>
2022-08-29 10:11:30 -0600

    Add DB seeds (characters and locations)

---
+++ db/migrate/20220829153935_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[7.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :item
+
+      t.references :location, foreign_key: true, index: true
+
+      t.timestamps
+    end
+  end
+end
