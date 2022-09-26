0411d0bee0a9001efe6e65eeead70c63fb45e5c0
Warren <hello@warren.codes>
2022-09-26 09:16:30 -0600

    Install Rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-puts "Starting seeding process..."
-
-puts "Creating locations:"
-
-10.times do
-    Location.create(name: Faker::Games::Zelda.location)
-end
-
-puts "Finished locations."
-
-puts "Creating characters:"
-
-locations = Location.all # An array of ALL locations in the DB!
-
-30.times do
-    Character.create(
-        name: Faker::Games::Zelda.character,
-        item: Faker::Games::Zelda.item,
-        location: locations.sample # Grab one random location from our array.
-    )
-end
-
-puts "Finished characters."
-
-puts "Seeding complete."
