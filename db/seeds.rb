ca1870e82537972e1b491157c4efc5ac8ca110aa
Warren <hello@warren.codes>
2022-09-26 10:02:15 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Beginning seeding process..."
+
+puts "Building cities..."
+
+12.times do
+    City.create(
+        name: Faker::Games::DnD.city,
+        language: Faker::Games::DnD.language
+    )
+end
+
+puts "Cities standing tall!"
+
+puts "Birthing monsters..."
+
+all_cities = City.all
+
+50.times do
+    Monster.create(
+        name: Faker::Games::DnD.monster,
+        melee_weapon: Faker::Games::DnD.melee_weapon,
+        ranged_weapon: Faker::Games::DnD.ranged_weapon,
+        level: rand(1..100),
+        city: all_cities.sample # Returns one random city
+    )
+end
+
+puts "Monsters ready for combat!"
+
+puts "Seeding complete."
