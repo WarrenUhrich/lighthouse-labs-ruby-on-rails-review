a063d874818b6c4da9776a03fe58598993fb42f5
Warren <hello@warren.codes>
2023-08-01 17:49:37 -0600

    Create new Rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-puts "Generating Locations..."
-
-10.times do
-    Location.create(
-        name: Faker::Fantasy::Tolkien.location
-    )
-end
-
-puts "Locations are done generating."
-
-puts "Generating Characters..."
-
-locations = Location.all
-
-50.times do
-    Character.create(
-        name: Faker::Fantasy::Tolkien.character,
-        favourite_poem: Faker::Fantasy::Tolkien.poem,
-        location: locations.sample
-    )
-end
-
-puts "Done character generation."
-
-puts "Done seeding."
