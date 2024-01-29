26710aefb5233e41f49e848ad9d584d21a09100f
Warren Uhrich <hello@warren.codes>
2024-01-29 11:07:40 -0700

    Reset folder

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :characters, only: [:index] # except: []
-  resources :locations, only: [:index, :show]
-end
